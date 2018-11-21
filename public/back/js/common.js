716bdd4d4a25faa855269ba5c4710a10c8a414f0
Albert-sxj <15900697165@163.com>
2018-11-22 00:30:55 +0800

    公共的功能

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -1,4 +1,8 @@
 $(function () {
+
+ 
+
+
     // NProgress.start();
     // NProgress .done();
     $(document).ajaxStart(function () {
@@ -14,4 +18,63 @@ $(function () {
     });
 
 
+    /* 公共 */
+
+/* 1 左侧二级切换 */
+/* 2 左侧菜单切换 */
+/* 公共退出功能 */
+
+$('#category').click (function() {
+    // $(this).next().stop().slideDown();
+    $(this).next().stop().slideToggle();
+
 })
+
+$('.icon_left').click (function() {
+    // $(this).next().stop().slideDown();
+    $(".aside").toggleClass('hidemenu');
+    $(".lt_main").toggleClass('hidemenu');
+    $(".lt_main .header").toggleClass('hidemenu');
+
+
+})
+
+
+// 3 退出功能
+
+$('.lt_main .inco-right').click (function() {
+
+
+  $('#myModal').modal('show')
+ 
+    
+})
+
+
+$('#logoutBtn').click(function() {
+    
+  $.ajax({
+    type:"get",
+    url:'/employee/employeeLogout',
+    dateType:"json",
+    success:function(info) {
+            if (info.success) {
+                location.href = "login.html";
+            }
+    }
+
+
+
+
+  })
+
+})
+
+
+
+
+
+
+
+})
+
---
+++ public/back/js/isLogin.js
@@ -0,0 +1,14 @@
+$.ajax( {
+    type:'get',
+    url:'/employee/checkRootLogin',
+    dataType:"json",
+    success:function(info) {
+         if(info.success) {
+
+         }
+         if(info.error === 400) {
+             location.href = "login.html"
+         }
+         
+
+ }})
