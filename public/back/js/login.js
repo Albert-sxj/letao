73eea45055a03628b10c94cff58a70ddd75e6c1a
Albert-sxj <15900697165@163.com>
2018-11-21 19:07:14 +0800

    登录功能完成

--- public/back/js/login.js
+++ public/back/js/login.js
@@ -25,6 +25,13 @@ $(function () {
                         max: 8,
                         message: '用户名长度必须在2到8之间'
                     },
+                    callback: {
+                        message: "用户名不存在",
+
+
+                    }
+                    // 专门用于ajax提示的
+
                     // //正则校验
                     // regexp: {
                     //     regexp: /^[a-zA-Z0-9_\.]+$/,
@@ -44,6 +51,11 @@ $(function () {
                         max: 8,
                         message: '密码长度必须在5到12之间'
                     },
+                    callback: {
+                        message: "密码错误",
+
+
+                    }
                     // //正则校验
                     // regexp: {
                     //     regexp: /^[a-zA-Z0-9_\.]+$/,
@@ -63,30 +75,30 @@ $(function () {
         $.ajax({
             type: "post",
             url: '/employee/employeeLogin',
-            data:$("#form").serialize(),
-            dataType:'json',
-            success:function(info) {
+            data: $("#form").serialize(),
+            dataType: 'json',
+            success: function (info) {
                 console.log(info);
-                if(info.success) {
+                if (info.success) {
                     location.href = "index.html"
                 }
-                if(info.error ===1000) {
-                    alert(info.message)
+                if (info.error === 1000) {
+                    $("#form").data('bootstrapValidator').updateStatus("username", "INVALID", "callback")
                 }
-                if(info.error === 1001) {
-                    alert(info.message)
+                if (info.error === 1001) {
+                    $("#form").data('bootstrapValidator').updateStatus("password", "INVALID", "callback")
                 }
-                
+
             }
         })
 
 
     });
 
-    $('[type=reset]').on('click',function() {
+    $('[type=reset]').on('click', function () {
 
         $("#form").data('bootstrapValidator').resetForm(true);
-        
+
 
     })
 
